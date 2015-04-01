27eccef1911f37f449cc9395e9a022c9b33b1d80
Yoeri Van Damme <yoeri.vandamme@gmail.com>
2015-04-01 10:06:28 +0200

    prevented console window from closing

--- ExpressionTrees/Program.cs
+++ ExpressionTrees/Program.cs
@@ -9,6 +9,7 @@ namespace ExpressionTrees
 		{
 			Queries.FilterOnInclude<Car, Wheel>(car => car.Wheels, wheel => wheel.SizeInInches == 14)
 				.ForEach(car => Console.WriteLine($"car : {car.Name} wheels: {car.Wheels.Count}"));
+			Console.ReadKey();
 		}
 	}
 }
